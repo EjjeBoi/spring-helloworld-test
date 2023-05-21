52f985c04bfd7291df9bddae7bcd0d23fd87cdc4
EjjeBoi <ejje.ejje@list.ru>
2023-05-21 14:07:30 +0300

    Added new project files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,27 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        ApplicationContext applicationContext1 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+
+        ApplicationContext applicationContext2 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat bean2 =
+                (Cat) applicationContext.getBean("cat");
+
+        ApplicationContext applicationContext3 =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        Cat bean3 =
+                (Cat) applicationContext.getBean("cat");
+
+        boolean isEqualHelloWorld = bean.equals(bean1);
+        System.out.println("Equal: " + isEqualHelloWorld);
+
+        boolean isEqualCat = bean2.equals(bean3);
+        System.out.println("Equal: " + isEqualCat);
     }
 }
